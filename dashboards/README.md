ec207e2251bdc348fc0fd88d1e0c29a87bad40c8
Eric Pugh <epugh@opensourceconnections.com>
2024-12-17 10:08:40 -0500

    Align with existing search-quality-evaluation-dashboards patterns
    Signed-off-by: Eric Pugh <epugh@opensourceconnections.com>
    Signed-off-by: Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>

--- dashboards/README.md
+++
@@ -1,4 +0,0 @@
-# How to load dashboards into your Open Search installation
-
-Run the following shell script: `install_dashboards.sh`
-
---
+++ ubi-dashboards/install_dashboards.sh
@@ -0,0 +1,26 @@
+
+# Ansi color code variables
+ERROR='\033[0;31m'
+MAJOR='\033[0;34m'
+MINOR='\033[0;37m    '
+RESET='\033[0m' # No Color
+
+opensearch=$1
+opensearch_dashboard=$2
+set -eo pipefail
+
+if [ -z "$opensearch" ]; then
+    echo "Error: please pass in both the opensearch url and the opensearch dashboard url"
+    exit 1
+fi
+if [ -z "$opensearch_dashboard" ]; then
+    echo "Error: please pass in both the opensearch url and the opensearch dashboard url"
+    exit 1
+fi
+
+echo "${MAJOR}Using Open Search and Open Search Dashboards at $opensearch and $opensearch_dashboard respectively.${RESET}"
+
+echo "${MAJOR}\nInstalling Quality Evaluation Framework Dashboards${RESET}"
+curl -X POST "http://$opensearch_dashboard/api/saved_objects/_import?overwrite=true" -H "osd-xsrf: true" --form file=@ubi_dashboard.ndjson > /dev/null
+
+echo "${MAJOR}The UBI Dashboards were successfully installed${RESET}"
