b85a0c2a88504a5774e711dc8d35109b22fe0cc4
Eric Pugh <epugh@opensourceconnections.com>
2025-07-21 06:50:06 -0400

    Relocate scripts useful in testing.
    Signed-off-by: Eric Pugh <epugh@opensourceconnections.com>

--- src/test/scripts/msearch.sh
+++ src/test/scripts/msearch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# An example multi search UBI search of the ecommerce index.
+
 curl -s -X GET "http://localhost:9200/_msearch" -H 'Content-Type: application/json' -d'
 { "index": "ecommerce"}
 { "query": { "match_all": {} }, "ext": { "ubi": { "query_id": "11111" } } }
