cfe9289712d8f42cf27fd0061b35e492ccbd65f2
Eric Pugh <epugh@opensourceconnections.com>
2025-07-21 09:25:47 -0400

    Relocate scripts that are used for testing into src/test/scripts (#111)
    ---------
    
    Signed-off-by: Eric Pugh <epugh@opensourceconnections.com>

--- src/test/scripts/initialize-ubi.sh
+++ src/test/scripts/initialize-ubi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# Delete and recreate the ubi indexes using the schema defined in the plugin
+ 
 curl -s -X DELETE http://localhost:9200/ubi_queries,ubi_events | jq
 
 curl -s -X POST http://localhost:9200/_plugins/ubi/initialize | jq
